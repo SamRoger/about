78c7db14bfe2471b39e1255dcdb756639ca00084
Sam Almeida <samRAlmeida@icloud.com>
2017-05-20 18:24:27 -0400

    make it better

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
 
 TweenMax.staggerFrom(".hello", 3, {opacity:0, delay:1}, 2);
 TweenMax.staggerFrom(".world", 3, {opacity:0, delay:2}, 2);
-TweenMax.staggerFrom(".period", 3, {opacity:0, delay:6}, 2);
+TweenMax.staggerFrom(".period", 3, {opacity:0, delay:6.5}, 2);
 TweenMax.staggerFrom(".bio", 3, {opacity:0, delay:3}, 2);
-TweenMax.staggerFrom(".main_pic", 5, {opacity:0, left:300, ease:Power4.easeOut, delay:4.5});
+TweenMax.staggerFrom(".main_pic", 5, {opacity:0, left:300, ease:Power4.easeOut, delay:4});
 //photos js
-TweenMax.staggerFrom(".pic", 0.5, {opacity:0, y:200, rotation:180, delay:0.2}, 0.3);
+TweenMax.staggerFrom(".pic", 0.5, {opacity:0, y:200, rotation:180, delay:0.2}, 0.2);
 TweenMax.staggerFrom(".containerContact", 4, {opacity:0,})
 
 $(".pic").on("click", function(){
@@ -22,6 +22,18 @@ var modal = new Custombox.modal({
 modal.open();
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
