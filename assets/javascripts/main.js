35c83e2b2ec7ffd4030f3eea560777645689c642
Sam Almeida <samRAlmeida@icloud.com>
2017-05-21 00:11:37 -0400

    reworked without frameworks

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,48 +1,15 @@
-$(document).ready(function(){
+var photos = document.getElementById("photos");
+var contact = document.getElementById("contact");
+var home = document.getElementById("home");
 
+home.addEventListener("click", function() {
+	home.style.backgroundColor = '#fff';
+	home.style.color = '#000'
+	home.style.textShadow = 'none'
+})
 
-TweenMax.staggerFrom(".hello", 3, {opacity:0, delay:1}, 2);
-TweenMax.staggerFrom(".world", 3, {opacity:0, delay:2}, 2);
-TweenMax.staggerFrom(".period", 3, {opacity:0, delay:6.5}, 2);
-TweenMax.staggerFrom(".bio", 3, {opacity:0, delay:3}, 2);
-TweenMax.staggerFrom(".main_pic", 5, {opacity:0, left:300, ease:Power4.easeOut, delay:4});
-//photos js
-TweenMax.staggerFrom(".pic", 0.5, {opacity:0, y:200, rotation:180, delay:0.2}, 0.2);
-TweenMax.staggerFrom(".containerContact", 4, {opacity:0,})
-
-$(".pic").on("click", function(){
-var modal = new Custombox.modal({
-  content: {
-    effect: 'rotate',
-    target: '.picShow'
-  }
-});
-
-// Open
-modal.open();
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
+photos.addEventListener("click", function() {
+	photos.style.backgroundColor = '#fff';
+	photos.style.color = '#000'
+	photos.style.textShadow = 'none'
+})
